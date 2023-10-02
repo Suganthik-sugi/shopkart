34d7bfd1c5f550925c56f8f20b1fb4896ac403f9
sugikrish <suginkl@gmail.com>
2023-10-02 07:42:58 +0530

    coding

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from . models import Catagory
 
 def home(request):
     return render(request,"shop/index.html")
+
+def collection(request):
+    category=Catagory.objects.all()
+    return render(request,"shop/collection.html",{"category":category})
